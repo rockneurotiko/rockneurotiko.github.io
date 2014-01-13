a37f9c88c85660acb4e788052d34bda2685aebb4
Rock Neurotiko <miguelglafuente@gmail.com>
2014-01-13 21:37:31 +0100

    aoe

--- ApssFire/kdbReader/kdbReader/browser.js
+++ ApssFire/kdbReader/kdbReader/browser.js
@@ -1,25 +1,3 @@
-/* Copyright (C) 2012 Richard Mitchell
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 /*jslint unparam: true, white: true, browser: true */
 /*global FileReader: true, File: true, alert: true, jQuery: true */
 (function ($) {
@@ -41,7 +19,8 @@
                 return false;
             }
             var kdb_file = storage.get(file);
-            pdf_file.onerror = function() {
+            kdb_file.onerror = function() {
+            	$('#open').attr('disabled', null);
                 console.error("Error in: ", kdb_file.error.name);
             };
             
@@ -216,14 +195,29 @@
             }
         }
 
+		openButton = document.querySelector("#open");
+		openButton.addEventListener('click', function(){
+			process();
+		});
+		
+        /*
         $('#open').bind('click', function (ev) {
             ev.preventDefault();
             process();
         });
+        */
+        
+        keepassOpen = document.querySelector("#keepassopenform");
+		keepassOpen.addEventListener('submit', function(){
+			process();
+		});
+        
+        /*
         $('#keepassopenform').bind('submit', function (ev) {
             ev.preventDefault();
             process();
         });
+        */
 
         $('#db-contents').on('click', 'a.group-name', function (ev) {
             ev.preventDefault();
