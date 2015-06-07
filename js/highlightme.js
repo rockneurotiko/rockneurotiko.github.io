d75878d327b0db1c75505b92ab8ddebaaff79dee
Rock Neurotiko <miguelglafuente@gmail.com>
2015-06-07 12:11:23 +0200

    fix

--- js/highlightme.js
+++ js/highlightme.js
@@ -4,15 +4,18 @@ function get(name){
     else return '';
 }
 
-var code=get("code");
-var lang=get("language");
-var theme=get("theme");
-var codec = document.getElementById("mycode");
+window.onload = function() {
+    var code=get("code");
+    var lang=get("language");
+    var theme=get("theme");
+    var codec = document.getElementById("mycode");
 
-// Right now don't do anything with theme
+    // Right now don't do anything with theme
 
-codec.textContent = code;
+    codec.textContent = code;
 
-if(lang !== "" && lang !== "detect" && lang !== "autodetect") {
-    codec.className = lang;
-}
+    if(lang !== "" && lang !== "detect" && lang !== "autodetect") {
+        codec.className = lang;
+    }
+    hljs.initHighlightingOnLoad();
+};
