a3aba8ab2d745412aa946f5dde9c8df7ce19f55b
Rock Neurotiko <miguelglafuente@gmail.com>
2015-06-07 12:58:59 +0200

    moar styles

--- js/highlightme.js
+++ js/highlightme.js
@@ -1,4 +1,9 @@
-/*global hljs */
+/*global hljs, $ */
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
 function get(name){
     var namel = (new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)'));
     namel = namel.exec(window.location.search);
@@ -7,22 +12,38 @@ function get(name){
     else return '';
 }
 
+function selectStyle(style) {
+    $('link[title]').each(function(i, link) {
+        link.disabled = (link.title != style);
+    });
+}
+
+function listStyles() {
+    var l = [];
+    $('link[title]').each(function(i, link) {
+        l.append(link.title);
+    });
+};
+
 var initialize = function initialize() {
     var code=get("code");
     var lang=get("language");
     var theme=get("theme");
     var codec = document.getElementById("mycode");
 
-    // Right now don't do anything with theme
-
     codec.textContent = code;
 
     if(lang !== "" && lang !== "detect" && lang !== "autodetect") {
         codec.className = lang;
     }
+    // Right now don't do anything with theme
+    if (theme === '')
+        theme = 'monokai sublime';
+    selectStyle(theme.capitalizeFirstLetter());
+
     hljs.initHighlighting.called = false;
     hljs.initHighlighting();
-    hljs.highlightBlock(codec);
+    // hljs.highlightBlock(codec);
 };
 
 if(window.attachEvent) {
