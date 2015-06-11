0a8e0c6b065a829019f57b3b52e410417e33b06b
Rock Neurotiko <miguelglafuente@gmail.com>
2015-06-11 10:07:41 +0200

    More things

--- js/highlightme.js
+++ js/highlightme.js
@@ -17,10 +17,22 @@ function get(name){
     else return '';
 }
 
+
+function scrollToDrop(drop) {
+    var pos = $(drop + ' li.active').position().top;
+    if (pos > 100) {
+        $(drop).scrollTop(pos - 100);
+    }
+}
+
 function selectStyle(style) {
     $('link[title]').each(function(i, link) {
         link.disabled = (link.title != style);
     });
+    $("#stylesdrop li").removeClass("active");
+    $("#stylesdrop").find("li:contains(\"" + style + "\")").addClass("active");
+    var t = $('#stylesdrop li.active');
+    var t2 = t.position().top;
 }
 
 function listStyles() {
@@ -93,7 +105,7 @@ function setStylesDrop() {
     var langs = listStyles();
     for (var i in langs) {
         var l = langs[i];
-        langsdrop.append('<li><a href="#" onclick="selectStyle(\'' + l + '\');">' + l + '</a></li>');
+        langsdrop.append('<li><a href="#" onclick="selectStyle(\'' + l.capitalizeFirstLetter() + '\');">' + l + '</a></li>');
     }
 };
 
@@ -107,6 +119,22 @@ function setLanguagesDrop() {
     }
 };
 
+function initializeListeners() {
+    $(".dropdown-toggle").attrchange({
+        trackValues: true, // set to true so that the event object is updated with old & new values
+        callback: function(evnt) {
+            if(evnt.attributeName == "aria-expanded") { // which attribute you want to watch for changes
+                if(evnt.newValue.search(/'true'/i) == -1) {
+                    scrollToDrop('#stylesdrop');
+                }
+            }
+        }
+    });
+    $("#stylesdrop").on('show', function() {
+        scrollToDrop('#stylesdrop');
+    });
+}
+
 var initialize = function initialize() {
     var code=get("code");
     var lang=get("language") || get("lang");
@@ -114,6 +142,11 @@ var initialize = function initialize() {
     var codec = document.getElementById("mycode");
 
     codec.textContent = code;
+
+    setLanguagesDrop();
+    setStylesDrop();
+
+
 
     if(lang !== "" && lang !== "detect" && lang !== "autodetect") {
         codec.className = lang;
@@ -126,10 +159,9 @@ var initialize = function initialize() {
     rehighlight();
     if (code === '') {
         toggleedit();
-
     }
-    setLanguagesDrop();
-    setStylesDrop();
+
+    initializeListeners();
 };
 
 if(window.attachEvent) {
